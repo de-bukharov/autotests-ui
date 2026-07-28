bbee6ab0138354d4680ea522df3ddccf4db87966
Dmitiry Bukharov <135876613+de-bukharov@users.noreply.github.com>
2026-07-28 15:25:30 +0400

    Паттерны автоматизации тестирования UI. Практикуемся в реализации PageObject

--- pages/create_course_page.py
+++ pages/create_course_page.py
@@ -43,9 +43,10 @@ class CreateCoursePage(BasePage):
 
 
 
-    def check_visible_create_course_tile(self):
-        expect(self.create_course_title_input).to_be_visible()
-        expect(self.create_course_title_input).to_have_text("Create Course")
+    def check_visible_create_course_title(self):
+        expect(self.create_course_title).to_be_visible()
+        expect(self.create_course_title).to_have_text('Create course')
+
 
     def click_create_course_button(self):
         self.create_course_button.click()
