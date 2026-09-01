4c3ba5e007a2d705ca74028bc245800cab08af54
Dmitiry Bukharov <135876613+de-bukharov@users.noreply.github.com>
2026-09-01 10:25:07 +0400

    Практикуемся в реализации PageComponent

--- tests/test_courses.py
+++ tests/test_courses.py
@@ -1,6 +1,4 @@
 import pytest
-from playwright.sync_api import sync_playwright, expect, Page
-import pytest
 
 from pages.courses_list_page import CoursesListPage
 from pages.create_course_page import CreateCoursePage
@@ -11,7 +9,7 @@ def test_empty_courses_list(courses_list_page: CoursesListPage):
         courses_list_page.visit("https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses")
         courses_list_page.navbar.check_visible('username')
         courses_list_page.sidebar.check_visible()
-        courses_list_page.check_visible_courses_title()
+        courses_list_page.toolbar_view.check_visible()
         courses_list_page.check_visible_empty_view()
 
 
@@ -28,10 +26,9 @@ def test_create_course(create_course_page: CreateCoursePage, courses_list_page: 
         create_course_page.check_visible_exercises_empty_view()
         create_course_page.upload_preview_image("./testdata/files/image.png")
         create_course_page.check_visible_image_upload_view(is_image_uploaded=True)
-        create_course_page.fill_create_course_form(title="Playwright" ,estimated_time="2 weeks", description="Playwright", max_score="100", min_score="10")
+        create_course_page.fill_create_course_form(title="Playwright", estimated_time="2 weeks", description="Playwright", max_score="100", min_score="10")
         create_course_page.click_create_course_button()
-        courses_list_page.check_visible_courses_title()
-        courses_list_page.check_visible_create_course_button()
-        courses_list_page.check_visible_course_card(0,"Playwright", "100", "10", "2 weeks")
+        courses_list_page.toolbar_view.check_visible()
+        courses_list_page.course_view.check_visible(index=0, title="Playwright", max_score="100", min_score="10", estimated_time="2 weeks")
 
 
