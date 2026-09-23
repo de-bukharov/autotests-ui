32a9906d37a9d09d79e32baa769275cce3f967a8
Dmitiry Bukharov <135876613+de-bukharov@users.noreply.github.com>
2026-09-23 15:57:57 +0400

    Паттерны автоматизации тестирования UI. Практическое применение PageFactory в автотестах

--- components/courses/create_course_form_component.py
+++ components/courses/create_course_form_component.py
@@ -1,19 +1,20 @@
 from playwright.sync_api import Page, expect
 from components.base_component import BaseComponent
-
+from elements.textarea import TextArea
+from elements.input import Input
 
 class CreateCourseFormComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.create_course_title_input = page.get_by_test_id("create-course-form-title-input").locator("input")
-        self.create_course_estimated_time_input = page.get_by_test_id(
-            "create-course-form-estimated-time-input").locator("input")
-        self.create_course_description_textarea = page.get_by_test_id(
-            "create-course-form-description-input").locator("textarea").first
+        self.create_course_title_input = Input(page, locator="create-course-form-title-input", name="Title input")
+        self.create_course_estimated_time_input = Input(
+            page, locator="create-course-form-estimated-time-input", name="Estimated time input")
+        self.create_course_description_textarea = TextArea(
+            page, locator="create-course-form-description-input", name="Description textarea")
 
-        self.create_course_max_score_input = page.get_by_test_id("create-course-form-max-score-input").locator("input")
-        self.create_course_min_score_input = page.get_by_test_id("create-course-form-min-score-input").locator("input")
+        self.create_course_max_score_input = Input(page, locator="create-course-form-max-score-input", name="Max score input")
+        self.create_course_min_score_input = Input(page, locator="create-course-form-min-score-input", name="Min score input")
 
 
     def check_visible(
@@ -24,20 +25,20 @@ class CreateCourseFormComponent(BaseComponent):
             max_score: str,
             min_score: str
     ):
-        expect(self.create_course_title_input).to_be_visible()
-        expect(self.create_course_title_input).to_have_value(title)
+        self.create_course_title_input.check_visible()
+        self.create_course_title_input.check_have_value(title)
 
-        expect(self.create_course_estimated_time_input).to_be_visible()
-        expect(self.create_course_estimated_time_input).to_have_value(estimated_time)
+        self.create_course_estimated_time_input.check_visible()
+        self.create_course_estimated_time_input.check_have_value(estimated_time)
 
-        expect(self.create_course_description_textarea).to_be_visible()
-        expect(self.create_course_description_textarea).to_have_value(description)
+        self.create_course_description_textarea.check_visible()
+        self.create_course_description_textarea.check_have_value(description)
 
-        expect(self.create_course_max_score_input).to_be_visible()
-        expect(self.create_course_max_score_input).to_have_value(max_score)
+        self.create_course_max_score_input.check_visible()
+        self.create_course_max_score_input.check_have_value(max_score)
 
-        expect(self.create_course_min_score_input).to_be_visible()
-        expect(self.create_course_min_score_input).to_have_value(min_score)
+        self.create_course_min_score_input.check_visible()
+        self.create_course_min_score_input.check_have_value(min_score)
 
 
 
--- components/courses/create_course_toolbar_view_component.py
+++ components/courses/create_course_toolbar_view_component.py
@@ -1,26 +1,28 @@
 from playwright.sync_api import Page, expect
 from components.base_component import BaseComponent
+from elements.text import Text
+from elements.button import Button
 
 
 class CreateCourseToolbarViewComponent(BaseComponent):
     def __init__(self, page: Page):
         super().__init__(page)
 
-        self.create_course_title = page.get_by_test_id("create-course-toolbar-title-text")
-        self.create_course_button = page.get_by_test_id("create-course-toolbar-create-course-button")
+        self.create_course_title = Text(page, locator="create-course-toolbar-title-text", name="Title")
+        self.create_course_button = Button(page, locator="create-course-toolbar-create-course-button", name="Create course")
 
 
 
 
     def check_visible(self, is_create_course_disabled: bool):
-        expect(self.create_course_title).to_be_visible()
-        expect(self.create_course_title).to_have_text('Create course')
-        expect(self.create_course_button).to_be_visible()
+        self.create_course_title.check_visible()
+        self.create_course_title.check_have_text('Create course')
+        self.create_course_button.check_visible()
         if is_create_course_disabled:
-            expect(self.create_course_button).to_be_disabled()
+            self.create_course_button.check_disabled()
 
         else:
-            expect(self.create_course_button).to_be_enabled()
+            self.create_course_button.check_enabled()
 
 
     def click_create_course_button(self):
