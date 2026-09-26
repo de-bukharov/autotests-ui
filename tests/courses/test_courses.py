00392443d5bcadaa828ec3bea6a5e4c81dc3d6b2
Dmitiry Bukharov <135876613+de-bukharov@users.noreply.github.com>
2026-09-26 22:30:07 +0400

    Паттерны автоматизации тестирования UI. Наращивание тестовой базы и рефакторинг

--- tests/courses/test_courses.py
+++ tests/courses/test_courses.py
@@ -34,3 +34,24 @@ class TestCourses:
                 courses_list_page.toolbar_view.check_visible()
                 courses_list_page.course_view.check_visible(index=0, title="Playwright", max_score="100",
                                                             min_score="10", estimated_time="2 weeks")
+
+
+        def test_edit_course(self, create_course_page: CreateCoursePage, courses_list_page: CoursesListPage):
+                create_course_page.visit(
+                        "https://nikita-filonov.github.io/qa-automation-engineer-ui-course/#/courses/create")
+                create_course_page.create_course_form.fill(title="Playwright", estimated_time="2 weeks",
+                                                           description="Playwright", max_score="100", min_score="10")
+                create_course_page.image_upload_widget.upload_preview_image('./testdata/files/image.png')
+                create_course_page.create_course_toolbar_view.click_create_course_button()
+                courses_list_page.course_view.check_visible(index=0, title="Playwright", max_score="100",
+                                                            min_score="10", estimated_time="2 weeks")
+                courses_list_page.course_view.menu.click_edit_menu_button(index=0)
+                create_course_page.create_course_form.fill(title="TestEditCourse", estimated_time="5 days",
+                                                           description="TestEditCourse", max_score="5", min_score="1")
+                create_course_page.create_course_toolbar_view.click_create_course_button()
+                courses_list_page.course_view.check_visible(index=0, title="TestEditCourse", max_score="5",
+                                                            min_score="1", estimated_time="5 days")
+
+
+
+
